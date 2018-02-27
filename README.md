161b01c876700c94ea5779abf12701561c477211
chriszielinski <chrisz@berkeley.edu>
2018-02-26 22:05:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Segue Demo for iOS DeCal Spring 18 Lecture 4
+
+**Completed** Demo Code for Lecture 4 of the iOS DeCal (creating segues and passing data between views).
---
+++ Segue Demo/MainViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  PupProfile
+//
+//  Created by Paige Plander on 2/19/17.
+//  Copyright © 2017 Paige Plander. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    @IBAction func unwindToMainViewConraewfwdsoller(segue: UIStoryboardSegue) {
+//
+//    }
+
+
+
+    /// Opens the user's library, allowing them to choose a photo
+    ///
+    /// - Parameter sender: The "Add Photo From Library" Button
+    @IBAction func chooseFromLibrary(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    /// Sets the Image View's image to the one chosen by the user
+    ///
+    /// - Parameters:
+    ///   - picker: The image picker (user's library)
+    ///   - info: data needed to create the image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        previewImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func viewNameAction(_ sender: Any) {
+        performSegue(withIdentifier: "showTextView", sender: sender)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toViewPhoto" {
+            if let photoViewController = segue.destination as? PhotoViewController {
+                photoViewController.imageToDisplay = previewImageView.image
+            }
+        } else if segue.identifier == "showTextView" {
+            if let textViewController = segue.destination as? TextViewController {
+                textViewController.stringToDisplay = nameTextField.text ?? ""
+            }
+        }
+    }
+    
+}
+
